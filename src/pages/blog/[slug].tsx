3ffd0eb91ab170a5dbc28342304a4f2a456dad83
Shawn Rivers <kaushou@gmail.com>
2020-11-23 00:33:02 +0900

    style: modify blog entry page styles

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -241,38 +241,40 @@ const RenderPost = ({ post, redirect, preview }) => {
             case 'video':
             case 'embed': {
               const { format = {} } = value;
+
               const {
                 block_width,
                 block_height,
                 display_source,
                 block_aspect_ratio,
               } = format;
-              const baseBlockWidth = 768;
-              const roundFactor = Math.pow(10, 2);
+              // const baseBlockWidth = 768;
+              // const roundFactor = Math.pow(10, 2);
               // calculate percentages
-              const width = block_width
-                ? `${
-                    Math.round(
-                      (block_width / baseBlockWidth) * 100 * roundFactor,
-                    ) / roundFactor
-                  }%`
-                : block_height || '100%';
+              // const width = block_width
+              //   ? `${
+              //       Math.round(
+              //         (block_width / baseBlockWidth) * 100 * roundFactor,
+              //       ) / roundFactor
+              //     }%`
+              //   : block_height || '100%';
 
               const isImage = type === 'image';
               const Comp = isImage ? 'img' : 'video';
-              const useWrapper = block_aspect_ratio && !block_height;
+              // const useWrapper = block_aspect_ratio && !block_height;
+              const useWrapper = block_aspect_ratio && !block_width;
               const childStyle: CSSProperties = useWrapper
                 ? {
                     width: '100%',
-                    height: '100%',
+                    // height: '100%',
                     border: 'none',
                     position: 'absolute',
                     top: 0,
                   }
                 : {
-                    width,
+                    width: block_width,
                     border: 'none',
-                    height: block_height,
+                    // height: block_height,
                     display: 'block',
                     maxWidth: '100%',
                   };
