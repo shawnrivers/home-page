c83ee6bff675171d0e8e668b6bd1a2384b5bcdff
Shawn Rivers <kaushou@gmail.com>
2020-11-23 17:07:20 +0900

    feat(home): clear up original home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,6 @@ import styles from '../styles/header.module.css';
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ];
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,24 @@
-import Link from 'next/link';
+import * as React from 'react';
 import Header from '../components/header';
-import ExtLink from '../components/ext-link';
-import Features from '../components/features';
-import GitHub from '../components/svgs/github';
-import sharedStyles from '../styles/shared.module.css';
+import homeStyles from '../styles/home.module.css';
+import Image from 'next/image';
 
-export default () => (
+const Home: React.FC = () => (
   <>
     <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
-      <img
-        src="/vercel-and-notion.png"
-        height="85"
+    <div className={homeStyles.layout}>
+      <Image
+        src="/avatar.jpg"
         width="250"
-        alt="Vercel + Notion"
+        height="250"
+        alt=""
+        role="presentation"
+        className={homeStyles.avatar}
       />
-      <h1>My Notion Blog</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/vercel/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
-
-      <Features />
-
-      <div className="explanation">
-        <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://vercel.com">Vercel</ExtLink>.
-          It leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/vercel/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
-        </p>
-      </div>
+      <h1>Yuxiao He's Blog</h1>
+      <h2>Writing about web front-end development.</h2>
     </div>
   </>
 );
+
+export default Home;
