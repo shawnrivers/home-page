2cbb932a0d8f94c5ecb1606a8eea3cd0e34727c8
Shawn Rivers <kaushou@gmail.com>
2020-12-18 18:54:36 +0900

    feat: fix footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,23 +1,21 @@
-import ExtLink from './ext-link';
+import * as React from 'react';
 
-export default () => (
-  <>
+const START_YEAR = 2020;
+
+const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
-        <img
-          src="https://vercel.com/button"
-          height={46}
-          width={132}
-          alt="deploy to Vercel button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
+      <p>
+        Copyright @
+        {currentYear === START_YEAR
+          ? currentYear
+          : `${START_YEAR}-${currentYear}`}{' '}
+        Usho Ka / Yuxiao He. All rights reserved.
+      </p>
     </footer>
-  </>
-);
+  );
+};
+
+export default Footer;
