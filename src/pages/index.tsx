12f10e49eb2e6ed3a3a79535161056c3e64c2e24
Shawn Rivers <kaushou@gmail.com>
2020-12-22 20:13:50 +0900

    style: fix home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,21 @@ const Home: React.FC = () => (
   <>
     <Header titlePre="Home" />
     <div className="text-center">
-      <Image
-        src="/avatar.jpg"
-        width="250"
-        height="250"
-        alt=""
-        role="presentation"
-        className="rounded-full"
-      />
-      <h1>Yuxiao He's Blog</h1>
-      <h2>Writing about web front-end development.</h2>
+      <div className="inline-block overflow-hidden rounded-full border-4 border-gray-800">
+        <Image
+          src="/avatar.jpg"
+          width="250"
+          height="250"
+          alt=""
+          role="presentation"
+        />
+      </div>
+      <div className="mx-auto mt-8">
+        <h1>Usho Ka / Yuxiao He</h1>
+        <div className="inline-block mt-4 p-8 rounded-lg shadow bg-gray-200 text-gray-800">
+          <h2>Nogizaka-driven web developer</h2>
+        </div>
+      </div>
     </div>
   </>
 );
