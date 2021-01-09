c523547c079ec028b7c852703b5b58068edb3b96
Shawn Rivers <kaushou@gmail.com>
2021-01-09 17:45:21 +0900

    style: extend minWidth in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
@@ -6,7 +7,7 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      ...colors,
+      ...defaultTheme.colors,
       react: {
         light: colors.blue[400],
         default: colors.blue[500],
@@ -22,6 +23,28 @@ module.exports = {
         default: colors.gray[800],
         dark: colors.gray[900],
       },
+    },
+    minWidth: {
+      ...defaultTheme.minWidth,
+      '3xs': '12rem',
+      '2xs': '16rem',
+      xs: '20rem',
+      sm: '24rem',
+      md: '28rem',
+      lg: '32rem',
+      xl: '36rem',
+      '2xl': '42rem',
+      '3xl': '48rem',
+      '4xl': '56rem',
+      '5xl': '64rem',
+      '6xl': '72rem',
+      '7xl': '80rem',
+      prose: '65ch',
+      'screen-sm': '640px',
+      'screen-md': '768px',
+      'screen-lg': '1024px',
+      'screen-xl': '1280px',
+      'screen-2xl': '1536px',
     },
   },
   variants: {
