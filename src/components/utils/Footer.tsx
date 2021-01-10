0cf3ba3e78644f574bb0858f4dafb3d702c58b14
Shawn Rivers <kaushou@gmail.com>
2021-01-10 12:10:59 +0900

    refactor: give footer component the ability to take className prop

--- src/components/utils/Footer.tsx
+++ src/components/utils/Footer.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import { joinClassNames } from '../../lib/utils/class';
 
 const START_YEAR = 2020;
 
-export const Footer: React.FC = () => {
+export const Footer: React.FC<{
+  className?: string;
+}> = props => {
   const currentYear = new Date().getFullYear();
+  const copyrightYears =
+    currentYear === START_YEAR ? currentYear : `${START_YEAR}-${currentYear}`;
 
   return (
-    <footer className="py-8 text-center">
+    <footer className={joinClassNames('py-8 text-center', props.className)}>
       <p className="text-base text-gray-700">
-        Copyright @
-        {currentYear === START_YEAR
-          ? currentYear
-          : `${START_YEAR}-${currentYear}`}{' '}
-        Usho Ka (Yuxiao He). All rights reserved.
+        Copyright @ {copyrightYears} Usho Ka (Yuxiao He). All rights reserved.
       </p>
     </footer>
   );
