6d356a0000760b0f103fe1947f8c72b0ba9422b5
Shawn Rivers <kaushou@gmail.com>
2021-01-10 12:10:20 +0900

    refactor: add types to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
+import * as React from 'react';
+import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import 'katex/dist/katex.css';
 import { Footer } from '../components/utils/Footer';
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-);
+const App: React.FC<AppProps> = props => {
+  const { Component, pageProps } = props;
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
