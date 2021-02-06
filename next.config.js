ea5c3790de59d45f71f1978d124324aa4897280e
Shawn Rivers <kaushou@gmail.com>
2021-02-06 12:57:30 +0900

    build: remove unnecessary configs

--- next.config.js
+++ next.config.js
@@ -3,13 +3,5 @@ const withMDX = require('@next/mdx')({
 });
 
 module.exports = withMDX({
-  target: 'experimental-serverless-trace',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  webpack(cfg, { dev, isServer }) {
-    if (dev || !isServer) return cfg;
-
-    // we're in build mode so enable shared caching for Notion data
-    process.env.USE_CACHE = 'true';
-    return cfg;
-  },
 });
