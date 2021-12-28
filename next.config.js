dfc7bdac973c1133f12a9f9ae0585081362b0b12
Shawn Rivers <kaushou@gmail.com>
2021-12-28 19:03:36 +0900

    Swap React with Preact in the client production build

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['tsx', 'mdx'],
+  webpack: (config, { dev, isServer }) => {
+    
+    // Swap React with Preact in the client production build
+    // @see https://youtu.be/R59e1Vl5lO8?t=177
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
 });
