60181a68c74d4f40e97e27e851e8705c2261f013
Shawn Rivers <kaushou@gmail.com>
2022-01-30 12:18:42 +0900

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Page } from 'components/shared/Page';
+import type { NextPage } from 'next';
+
+const NotFound: NextPage = () => {
+  return (
+    <Page titlePre="Not Found">
+      <h1 className="text-center font-bold text-3xl">Not Found</h1>
+    </Page>
+  );
+};
+
+export default NotFound;
