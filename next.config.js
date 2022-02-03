abed0e5fafe65a6006b845af30b5b08748704b26
Shawn Rivers <kaushou@gmail.com>
2022-02-04 00:33:42 +0900

    Add AVIF support

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withMDX = require('@next/mdx')({
 
 module.exports = withMDX({
   pageExtensions: ['tsx', 'mdx'],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   webpack: (config, { dev, isServer }) => {
     // Swap React with Preact in the client production build
     // @see https://youtu.be/R59e1Vl5lO8?t=177
