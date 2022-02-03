feaf17e68335aebf6addf4499c597f94e18bd835
Shawn Rivers <kaushou@gmail.com>
2022-02-03 23:50:50 +0900

    Add image placeholder

--- src/components/pages/blog/Img.tsx
+++ src/components/pages/blog/Img.tsx
@@ -18,6 +18,7 @@ export const Img: React.FC<{
         height={props.height}
         unoptimized={props.unoptimized}
         priority={props.priority}
+        placeholder="blur"
         lazyBoundary="1000px"
         className="object-cover"
       />
