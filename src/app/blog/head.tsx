0cfc63546b154abad6901343ac85ba1601ae7c27
Shawn <kaushou@gmail.com>
2023-02-15 00:01:03 +0900

    fix: OG image path

--- src/app/blog/head.tsx
+++ src/app/blog/head.tsx
@@ -10,8 +10,8 @@ export default function Head() {
       <meta name="og:description" content="Usho's blog" />
       <meta name="og:type" content="website" />
       <meta name="og:url" content="https://usho.dev" />
-      <meta name="og:image" content="/og-image.png" />
-      <meta name="og:image:type" content="image/png" />
+      <meta name="og:image" content="/og-image.jpg" />
+      <meta name="og:image:type" content="image/jpeg" />
       <meta name="og:image:width" content="1280" />
       <meta name="og:image:height" content="640" />
       <meta name="og:image:alt" content="Usho's blog" />
