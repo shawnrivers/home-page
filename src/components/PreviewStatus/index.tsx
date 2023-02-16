03fef8678a8606de3d728eef08dea1c6e98bed55
Shawn <kaushou@gmail.com>
2023-02-17 01:44:42 +0900

    fix: improve preview status UI position

--- src/components/PreviewStatus/index.tsx
+++ src/components/PreviewStatus/index.tsx
@@ -15,7 +15,7 @@ export const PreviewStatus: React.FC<{ className?: string }> = ({
   return (
     <span
       className={cn(
-        'fixed bottom-2 right-8 flex items-center gap-2 rounded border-2 border-gray-800 bg-gray-200 px-2 py-1 font-bold dark:border-white dark:bg-gray-700',
+        'fixed bottom-2 left-4 flex items-center gap-2 rounded border-2 border-gray-800 bg-gray-200 px-2 py-1 font-bold dark:border-white dark:bg-gray-700',
         className,
       )}
     >
