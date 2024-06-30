d09aa74fa0de85286a61a05118f4f67afd2cfb50
Shawn <kaushou@gmail.com>
2024-07-01 00:22:41 +0900

    feat: add an API route for revalidation

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,27 @@
+import { revalidatePath } from 'next/cache';
+import { NextRequest } from 'next/server';
+import { z } from 'zod';
+
+const TypeSchema = z.union([z.literal('page'), z.literal('layout')]).nullable();
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const secret = searchParams.get('secret');
+  const path = searchParams.get('path');
+  const type = TypeSchema.parse(searchParams.get('type'));
+
+  if (secret !== process.env.REVALIDATE_API_SECRET) {
+    return new Response('Invalid token', { status: 401 });
+  }
+
+  if (path) {
+    revalidatePath(path, type ?? undefined);
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: 'Missing path to revalidate',
+  });
+}
