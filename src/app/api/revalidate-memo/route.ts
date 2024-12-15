4e8f74efc04b57b5519aeebeaed4246b8d5fd8db
Shawn <kaushou@gmail.com>
2024-12-16 02:40:45 +0900

    feat: improve revalidation route response

--- src/app/api/revalidate-memo/route.ts
+++ src/app/api/revalidate-memo/route.ts
@@ -6,26 +6,36 @@ export async function POST(request: NextRequest) {
   const secret = searchParams.get('secret');
 
   if (secret !== process.env.REVALIDATE_API_SECRET) {
-    return new Response('Invalid token', { status: 401 });
+    return Response.json(
+      {
+        revalidated: false,
+        message: 'Invalid token',
+      },
+      { status: 401 },
+    );
   }
 
   const content = await request.json();
 
   if (!content.Slug || typeof content.Slug !== 'string') {
-    return Response.json({
-      revalidated: false,
-      now: Date.now(),
-      message: 'Missing Slug',
-    });
+    return Response.json(
+      {
+        revalidated: false,
+        message: 'Missing Slug',
+      },
+      { status: 400 },
+    );
   }
 
   const path = `/memo/${content.Slug}`;
 
   revalidatePath(path);
 
-  return Response.json({
-    revalidated: true,
-    now: Date.now(),
-    message: `Revalidate ${path}`,
-  });
+  return Response.json(
+    {
+      revalidated: true,
+      message: `Revalidate ${path}`,
+    },
+    { status: 200 },
+  );
 }
