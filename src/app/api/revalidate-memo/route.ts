0fe529624f530177614b3b611a6bcfef3e44f9eb
Shawn <kaushou@gmail.com>
2024-12-16 02:15:39 +0900

    fix: update API route method

--- src/app/api/revalidate-memo/route.ts
+++ src/app/api/revalidate-memo/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const secret = searchParams.get('secret');
 
