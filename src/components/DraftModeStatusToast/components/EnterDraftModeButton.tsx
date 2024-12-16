38346902a5d11a1c9e3da3e3b906836e1df5ba6e
Shawn <kaushou@gmail.com>
2024-12-17 02:27:22 +0900

    refactor: refine API route naming

--- src/components/DraftModeStatusToast/components/EnterDraftModeButton.tsx
+++ src/components/DraftModeStatusToast/components/EnterDraftModeButton.tsx
@@ -5,7 +5,7 @@ export const EnterDraftModeButton: React.FC = () => {
   return (
     // eslint-disable-next-line @next/next/no-html-link-for-pages
     <a
-      href={`/api/draft?secret=${process.env.DRAFT_API_SECRET}`}
+      href={`/api/draft/enable?secret=${process.env.DRAFT_API_SECRET}`}
       className="bg-gray-800 px-2 py-1 text-sm text-white no-underline dark:bg-white dark:text-gray-800"
     >
       Enter
--- src/components/DraftModeStatusToast/components/ExitDraftModeButton.tsx
+++ src/components/DraftModeStatusToast/components/ExitDraftModeButton.tsx
@@ -2,7 +2,7 @@ export const ExitDraftModeButton: React.FC = () => {
   return (
     // eslint-disable-next-line @next/next/no-html-link-for-pages
     <a
-      href="/api/disable-draft"
+      href="/api/draft/disable"
       className="bg-gray-800 px-2 py-1 text-sm text-white no-underline dark:bg-white dark:text-gray-800"
     >
       Exit
