fcdc84731470e19e5e5bd642c4e40354d4f06ba0
Shawn <kaushou@gmail.com>
2024-12-31 12:19:27 +0900

    styles: modify footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: React.FC = () => {
     currentYear === startYear ? currentYear : `${startYear}-${currentYear}`;
 
   return (
-    <footer className="bg-gray-800 py-8 text-center">
+    <footer className="bg-gray-800 py-6 text-center">
       <p className="px-4 text-base text-white selection:bg-gray-600">
         Copyright @ {copyrightYears} Usho Ka (Yuxiao He). All rights reserved.
       </p>
