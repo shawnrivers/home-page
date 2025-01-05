78d96ff9615202732e35b4585fc03b1f8f6953da
Shawn <kaushou@gmail.com>
2025-01-05 18:24:18 +0900

    styles: refine colors

--- src/app/(home)/_components/LinkedList.tsx
+++ src/app/(home)/_components/LinkedList.tsx
@@ -14,7 +14,7 @@ export const LinkedList: React.FC<LinkedListProps> = ({ items, className }) => {
           <div
             className={cn(
               'absolute -left-2 top-0 box-border h-[1.125rem] w-[1.125rem] rounded-[50%] border-[4px] border-white dark:border-gray-800',
-              item.highlighted ? 'bg-red-500' : 'bg-gray-400',
+              item.highlighted ? 'bg-red-400' : 'bg-gray-400',
             )}
           />
           <div
