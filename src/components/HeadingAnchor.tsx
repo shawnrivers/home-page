28bf8867001c59cebf1ad73ad136552b7b52050f
Shawn <kaushou@gmail.com>
2025-03-26 01:55:52 +0900

    styles: refine colors

--- src/components/HeadingAnchor.tsx
+++ src/components/HeadingAnchor.tsx
@@ -3,7 +3,7 @@ import { convertNodeToString } from '@/libs/utils/string';
 
 type As = 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-const getBeforeContent = (as: As): string => {
+const getBeforeContent = (as: As) => {
   switch (as) {
     case 'h2':
       return 'before:content-["#"]';
@@ -35,7 +35,7 @@ export const HeadingAnchor: React.FC<HeadingAnchorProps> = ({
       >
         <span
           className={cn(
-            'uppercase tracking-wide before:mr-1 before:-tracking-widest before:text-gray-500 group-hover:before:text-gray-400',
+            'uppercase tracking-wide before:mr-1 before:-tracking-widest before:text-gray-500 group-hover:before:text-inherit',
             getBeforeContent(Component),
           )}
         >
