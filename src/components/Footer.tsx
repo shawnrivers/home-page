8aebb5de1595352f314172f35d5baa5ac4a6fd92
Shawn <kaushou@gmail.com>
2025-03-26 02:13:59 +0900

    styles: refine footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: React.FC = () => {
     currentYear === startYear ? currentYear : `${startYear}-${currentYear}`;
 
   return (
-    <footer className="bg-gray-800 py-6 text-center">
+    <footer className="bg-gray-800 py-4 text-center">
       <p className="px-4 text-base text-white selection:bg-gray-600">
         Copyright @ {copyrightYears} Usho Ka (Yuxiao He). All rights reserved.
       </p>
