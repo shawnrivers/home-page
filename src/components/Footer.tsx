3ca5a43acfa0ef202393131933f7a0fe4d239bd1
Shawn <kaushou@gmail.com>
2025-05-18 19:01:56 +0900

    styles: modify footer font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export const Footer: React.FC = () => {
     currentYear === startYear ? currentYear : `${startYear}-${currentYear}`;
 
   return (
-    <footer className="border-t-2 border-dashed text-gray-700 dark:text-gray-200 border-gray-400 dark:border-gray-600 pt-4 pb-[calc(env(safe-area-inset-bottom)+1rem)] pl-[calc(env(safe-area-inset-left)+1rem)] pr-[calc(env(safe-area-inset-right)+1rem)] text-center">
-      <p className="text-sm">
+    <footer className="border-t-2 border-dashed text-gray-700 dark:text-gray-200 border-gray-400 dark:border-gray-600 pt-4 pb-[calc(env(safe-area-inset-bottom)+1rem)] pl-[calc(env(safe-area-inset-left)+1rem)] pr-[calc(env(safe-area-inset-right)+1rem)]">
+      <p className="text-sm text-center font-serif">
         Copyright @ {copyrightYears} Usho Ka (Yuxiao He). All rights reserved.
       </p>
     </footer>
