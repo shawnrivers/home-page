51fd717b5120d133ac5768b1b8c6ba0330730b02
Shawn <kaushou@gmail.com>
2025-12-13 19:20:41 +0900

    Update typography color

--- src/app/memo/[slug]/page.tsx
+++ src/app/memo/[slug]/page.tsx
@@ -102,7 +102,7 @@ export default async function Post(props: MemoPageProps) {
             tableOfContents={tableOfContents}
           />
         )}
-        <article className="prose prose-gray relative w-full break-words lg:prose-lg dark:prose-invert prose-figcaption:mt-[0.5em] prose-pre:m-0 prose-h1:font-display prose-h2:font-display prose-h3:font-display">
+        <article className="prose prose-neutral relative w-full break-words lg:prose-lg dark:prose-invert prose-figcaption:mt-[0.5em] prose-pre:m-0 prose-h1:font-display prose-h2:font-display prose-h3:font-display">
           <div className="mb-8">
             <time
               dateTime={properties.Date.date.start}
